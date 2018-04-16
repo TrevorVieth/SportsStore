70c84c55c05cce2c57617f4f78411d58b3ba24ce
TrevorVieth <tdvieth1@buffs.wtamu.edu>
2018-04-15 21:32:45 -0500

    Added Chapter 10 content

--- SportsStore/Startup.cs
+++ SportsStore/Startup.cs
@@ -4,11 +4,14 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
-
 using SportsStore.Models;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+
+using Microsoft.AspNetCore.Http.Internal;
+
 
 namespace SportsStore
 {
@@ -28,6 +31,9 @@ namespace SportsStore
             services.AddDbContext<ApplicationDbContext>(options =>
                     options.UseSqlite("Data Source=SportsStore.db"));
             services.AddTransient<IProductRepository, EFProductRepository>();
+            services.AddScoped<Cart>(sp => SessionCart.GetCart(sp));
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            services.AddTransient<IOrderRepository, EFOrderRepository>();
             services.AddMemoryCache();
             services.AddSession();
 
